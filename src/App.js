89a2754fad3a2809a3be1e2bb385130a544276a7
marinellavar <mvarenova@gmail.com>
2022-11-13 16:27:00 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import {data} from './data';
+import React from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [visit, setVisit] = useState(data);
+  const removeVisit = (id) =>{
+    let newVisit = visit.filter(visit => visit.id !==id);
+    setVisit(newVisit)
+  }
+  return(
+    <div>
+      <div className='container'>
+        <h1>{visit.length} Things to do in Vienna</h1>
+
+      </div>
+      {visit.map((places =>{
+        const {id, location, image} = places;
+        return(
+          <div key={id}>
+            <div className='container'>
+              <p className='description'>{id} - {location}</p>
+            </div>
+            <div className='container'>
+            <img src = {image} width = '300px' alt='pic'/>
+            </div>
+
+            <div className='container'>
+              <button onClick={ () => removeVisit(id)}>Remove place</button>
+
+            </div>
+            
+            
+          </div>
+        )
+      }))}
+      <div className='container'>
+              <button onClick={ () => setVisit([])}>Delet all</button>
+
+            </div>
+            
+          
     </div>
-  );
+    
+  )
+ 
 }
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,40 @@
+export const data = [
+    {
+        id: 1,
+        location:'Stephansdom', 
+        image:'https://images.unsplash.com/photo-1572085313473-08e54ae31fc8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1935&q=80',
+
+    },
+
+    {
+        id: 2,
+        location:'Rathaus Der Stadt Wien', 
+        image:'https://images.unsplash.com/photo-1618325262870-03a840ed0a3d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1918&q=80',
+
+    },
+
+
+    {
+        id: 3,
+        location:'Karlskirche', 
+        image:'https://images.unsplash.com/photo-1592179896083-09083e2c2099?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1888&q=80',
+
+    },
+
+
+
+    {
+        id: 4,
+        location:'Wiener Staatsoper', 
+        image:'https://images.unsplash.com/photo-1591482036599-8f0b834a8e52?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1935&q=80',
+
+    },
+
+
+    {
+        id: 5,
+        location:'Prater', 
+        image:'https://images.unsplash.com/photo-1580910863380-ba56b443be86?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1887&q=80',
+
+    }
+]
